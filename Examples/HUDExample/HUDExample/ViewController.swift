d2a609e8e591bea4b9fb9cc088f9886e53ad25a9
Bei Li <kylinroc@gmail.com>
2021-05-01 00:21:08 +0800

    Update example

--- Examples/HUDExample/HUDExample/ViewController.swift
+++ Examples/HUDExample/HUDExample/ViewController.swift
@@ -4,11 +4,23 @@ import UIKit
 final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .add,
             target: self,
             action: #selector(showHUD)
         )
+
+        let backgroundImageView = UIImageView(image: UIImage(named: "iOS-14.2-wallpaper-LAke-The-Cliff"))
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backgroundImageView)
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
 
     @objc
